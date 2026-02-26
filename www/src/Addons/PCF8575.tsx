75e9f6b2327ed12182c203dae1a07a6042b56216
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-02-26 09:39:32 +0900

    Update PCF8575.tsx
    I/O

--- www/src/Addons/PCF8575.tsx
+++ www/src/Addons/PCF8575.tsx
@@ -22,6 +22,7 @@ import {
 	PinDirectionValues,
 } from '../Data/Pins';
 import { AddonPropTypes } from '../Pages/AddonsConfigPage';
+
 const SELECTABLE_BUTTON_ACTIONS = [
 	-10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
 	34, 35, 36, 37, 38, 39, // --- ここに Macro 1 ～ 6 の ID を追加 ---
