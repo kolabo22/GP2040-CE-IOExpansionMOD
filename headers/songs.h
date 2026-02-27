57b52680dcabbaa523788857ff10278f5b67f1fd
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-02-27 22:40:13 +0900

    Update songs.h
    SEGA

--- headers/songs.h
+++ headers/songs.h
@@ -6,11 +6,11 @@
 // セガの「セーガー」
 // 音程：G5(ソ) -> C5(ド) の本物に近い落差
 Song introSong{
-    80, // 1音を少し長めに設定
+    60, // 1音を少し長めに設定
     {
-        PAUSE, PAUSE, PAUSE,
-        G5, G5, G5, G5, G5, G5,       // 「セ」
-        PAUSE, PAUSE, PAUSE,          // 音の分離を良くする
+        PAUSE, 
+        G5, G5, G5, G5, G5, G5, G5,   // 「セ」
+        PAUSE, PAUSE,                 // 音の分離を良くする
         C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, PAUSE // 「ガー」
     }
 };
@@ -20,17 +20,16 @@ Song introSong{
 Song configModeSong{
     80,
     {
-        
+        PAUSE, 
         F4, F4, F4, F4, F4, F4, PAUSE, PAUSE, // 最初の重厚な響き（溜め）
         
         // 前半：上昇アルペジオ
-        C4, C4, E4, E4, G4, G4, B4, B4, C5, C5, E5, E5, G5, G5, B5, B5, C6, C6, D6, D6, E6, E6,
+        F4, F4, A4, A4, C5, C5, F5, F5, A5, A5, C6, C6,
         PAUSE, PAUSE, PAUSE, // 独特の「溜め」
 
         // 後半：広がる余韻（A2ではなくA3に変更してノイズを回避）
         // 音数を増やすことで、終わるのを遅くしています
-        A3, A3, A3, A3, A3, A3, A3, A3, A3, A3, A3, A3,
-        A3, A3, A3, A3, A3, A3, A3, A3, A3, A3, A3, A3
+        C5, C5, C5, C5, C5, C5, C5, C5, PAUSE
     }
 };
 
