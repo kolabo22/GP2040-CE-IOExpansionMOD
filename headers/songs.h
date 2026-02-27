3b8d8502f61ad42c3afb1d09424dab0d24a50a24
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-02-27 23:20:33 +0900

    Update songs.h
    SEGA

--- headers/songs.h
+++ headers/songs.h
@@ -8,28 +8,28 @@
 Song introSong{
     60, // 1音を少し長めに設定
     {
-        PAUSE, 
-        G5, G5, G5, G5, G5, G5, G5,   // 「セ」
+        PAUSE, PAUSE,
+        G5, G5, G5, G5, G5, G5, G5, G5, G5,  // 「セ」
         PAUSE, PAUSE,                 // 音の分離を良くする
-        C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, PAUSE // 「ガー」
+        C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, PAUSE // 「ガー」
     }
 };
 
 // サターン起動音（ロングエコー版）
 // 1単位を短く(25ms)して、音の密度を上げています
 Song configModeSong{
-    80,
+    40,
     {
         PAUSE, 
-        F4, F4, F4, F4, F4, F4, PAUSE, PAUSE, // 最初の重厚な響き（溜め）
+        F4, F4, F4, F4, F4, F4, F4, F4, F4, F4, F4, F4, PAUSE, // 最初の重厚な響き（溜め）
         
         // 前半：上昇アルペジオ
-        F4, F4, A4, A4, C5, C5, F5, F5, A5, A5, C6, C6,
+        F4, F4, F4, A4, A4, A4, C5, C5, C5, F5, F5, F5, A5, A5, A5, C6, C6, C6,
         PAUSE, PAUSE, PAUSE, // 独特の「溜め」
 
         // 後半：広がる余韻（A2ではなくA3に変更してノイズを回避）
         // 音数を増やすことで、終わるのを遅くしています
-        C5, C5, C5, C5, C5, C5, C5, C5, PAUSE
+        C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, C5, PAUSE
     }
 };
 
